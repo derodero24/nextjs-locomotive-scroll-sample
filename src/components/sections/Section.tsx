f1e14457e51f0d8931a45f771f260239d980005a
derodero24 <k.itsh20524abc@gmail.com>
2022-10-20 14:37:18 +0900

    update my icon

--- src/components/sections/Section.tsx
+++ src/components/sections/Section.tsx
@@ -5,8 +5,8 @@ export default function Section() {
     <section>
       <h1 className="text-3xl">Hello I&apos;m derodero👋</h1>
       <div className="my-6 flex space-x-8">
-        <div className="w-1/4">
-          <Image src="/icon-512.png" alt="icon" width={512} height={512} />
+        <div className="w-1/4 drop-shadow">
+          <Image src="/derodero24.png" alt="icon" width={460} height={460} />
         </div>
         <p className="w-3/4 text-lg">{'This is dummy text. '.repeat(20)}</p>
       </div>
