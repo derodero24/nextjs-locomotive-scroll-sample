bfc9c6901e059d5656d8b55acf7496f8b4398212
derodero24 <k.itsh20524abc@gmail.com>
2022-10-20 14:34:07 +0900

    remove translations

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 import GitHubButton from '../elements/GitHubButton';
-import LangButton from '../elements/LangButton';
 import ThemeButton from '../elements/ThemeButton';
 
 export default function Header() {
@@ -12,7 +11,6 @@ export default function Header() {
       </Link>
       <nav className="flex items-center space-x-6">
         <ThemeButton className="header-btn" />
-        <LangButton className="header-btn" />
         <GitHubButton className="header-btn" />
       </nav>
     </header>
