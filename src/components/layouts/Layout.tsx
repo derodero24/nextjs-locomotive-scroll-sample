46d8c27e754349eaf226205ca5866deba311098f
derodero24 <k.itsh20524abc@gmail.com>
2022-10-20 14:16:03 +0900

    add locomotive-scroll

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from 'react';
 
 export default function Layout(props: { children: ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div data-scroll-section className="flex min-h-screen flex-col">
       <Header />
       <main className="grow">{props.children}</main>
       <Footer />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import '../styles/global.css';
 
 import { ThemeProvider } from 'next-themes';
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
 
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { route } = useRouter();
+
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <LocomotiveScrollProvider options={{ smooth: true }} watch={[route]}>
+        <div data-scroll-container className="container" ref={containerRef}>
+          <Component {...pageProps} />
+        </div>
+      </LocomotiveScrollProvider>
     </ThemeProvider>
   );
 }
