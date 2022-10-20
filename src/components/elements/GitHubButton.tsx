644afb8bbcee2707e86718d8cb687acc278267dc
derodero24 <k.itsh20524abc@gmail.com>
2022-10-20 14:34:00 +0900

    change github url

--- src/components/elements/GitHubButton.tsx
+++ src/components/elements/GitHubButton.tsx
@@ -3,7 +3,7 @@ import { AiFillGithub } from 'react-icons/ai';
 export default function GitHubButton(props: { className: string }) {
   return (
     <a
-      href="https://github.com/derodero24"
+      href="https://github.com/derodero24/react-locomotive-scroll-sample"
       target="_blank"
       rel="noopener noreferrer"
     >
